57775279e4d67db7d0f2a1336e12355f20fb7255
andyrod <andyrod209@gmail.com>
2022-03-08 12:00:38 -0800

    changed working

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 // Displays Hello World and to learn about branching and tags!!!
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('Hello to Andy and the rest of the world out there!')
+    res.send('test!')
 }); app.listen(APP_PORT, () => {
     console.log(`Running app at port:${APP_PORT}`)
 })
